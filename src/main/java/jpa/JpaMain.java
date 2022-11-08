9d9f514e077cf4875d0abb87f91a41515b370528
김신영 <rolroralra@gmail.com>
2022-11-08 21:48:17 +0900

    refactor: reformat code

--- src/main/java/jpa/JpaMain.java
+++ src/main/java/jpa/JpaMain.java
@@ -3,70 +3,37 @@ package jpa;
 import domain.Member;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import java.util.List;
 
-public class JpaMain {
+public class JpaMain extends JpaMainTemplate {
+
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        new JpaMain().run("test");
+    }
 
-        EntityManager em = emf.createEntityManager();
+    @Override
+    public void logic(EntityManager em) {
+        Member member = new Member();
+        member.setUsername("root");
+        member.setRoleType(Member.RoleType.ADMIN);
 
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        Member member2 = new Member();
+        member2.setUsername("admin");
+        member2.setRoleType(Member.RoleType.ADMIN);
 
-        try {
-
-            Member member = new Member();
-//            member.setId("ID_A");
-//            member.setId(3L);
-            member.setUsername("root");
-            member.setRoleType(Member.RoleType.ADMIN);
-
-            Member member2 = new Member();
-            member2.setUsername("admin");
-            member2.setRoleType(Member.RoleType.ADMIN);
-
-            Member member3 = new Member();
-            member3.setUsername("guest");
-            member3.setRoleType(Member.RoleType.GUEST);
+        Member member3 = new Member();
+        member3.setUsername("guest");
+        member3.setRoleType(Member.RoleType.GUEST);
 
 
-            System.out.println("===== BEFORE PERSIST =====");
-            em.persist(member);
-            em.persist(member2);
-            em.persist(member3);
-            System.out.println("===== AFTER PERSIST =====");
-            System.out.println(member);
-            System.out.println(member2);
-            System.out.println(member3);
-//            Member member = new Member(1L, "root");
-//            em.persist(member);
-
-//            Member member = new Member(3L, "rolroralra");
-//            insertMember(em, member);
-//            updateMember(em, 3L, new Member(3L, "guest"));
-
-//            selectAllMembers(em).forEach(System.out::println);
-//            System.out.println(selectMember(em, 3L));
-
-            System.out.println("===== BEFORE COMMIT =====");
-            tx.commit();
-            System.out.println("===== AFTER COMMIT =====");
-
-        } catch (Exception e) {
-            System.out.println("===== BEFORE ROLLBACK =====");
-            tx.rollback();
-            e.printStackTrace();
-            System.out.println("===== AFTER ROLLBACK =====");
-        } finally {
-            em.close();
-        }
-
-
-        emf.close();
+        System.out.println("===== BEFORE PERSIST =====");
+        em.persist(member);
+        em.persist(member2);
+        em.persist(member3);
+        System.out.println("===== AFTER PERSIST =====");
+        System.out.println(member);
+        System.out.println(member2);
+        System.out.println(member3);
     }
 
     private static void insertMember(EntityManager em, Member member) {
@@ -88,4 +55,9 @@ public class JpaMain {
                 .getResultList();
     }
 
+    private static void deleteMember(EntityManager em, Long id) {
+        Member findMember = selectMember(em, id);
+
+        em.remove(findMember);
+    }
 }
